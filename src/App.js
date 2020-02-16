515ef114011ca24b5807e0d98da4fa8e359f3c48
Emma <129emma@gmail.com>
2020-02-16 20:21:20 +1300

    add travis ci

--- src/App.js
+++ src/App.js
@@ -2,6 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const testApp = () => {
+  return (
+      <div>Test</div>
+  )
+};
+
+
 function App() {
   return (
     <div className="App">
