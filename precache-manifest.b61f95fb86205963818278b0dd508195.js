de5554870c0b34105c232fdd60a82159dbe84ce1
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-02-16 07:28:23 +0000

    Deploy 129emma/test-app to github.com/129emma/test-app.git:gh-pages

---
+++ precache-manifest.b61f95fb86205963818278b0dd508195.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ec6fa2f9785a91762f517d0897f2faa",
+    "url": "/test-app/index.html"
+  },
+  {
+    "revision": "d0d78423114ebfbe992f",
+    "url": "/test-app/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "d60f1629cfd06c1c48be",
+    "url": "/test-app/static/js/2.6199a9ab.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/test-app/static/js/2.6199a9ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0d78423114ebfbe992f",
+    "url": "/test-app/static/js/main.79e9ba24.chunk.js"
+  },
+  {
+    "revision": "a7685168259b13e7085a",
+    "url": "/test-app/static/js/runtime-main.350dc61c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
